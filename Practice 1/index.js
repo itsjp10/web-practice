500a91060df987fba0e0942f5780f2e81131a2b0
Jean Pierre <jalvarezidep@gmail.com>
2025-12-21 00:43:13 -0500

    practice 1 with query selectors finished

---
+++ Practice 1/index.js
@@ -0,0 +1,16 @@
+
+let selectorTecnologia = document.querySelector("#tecnologia")
+let posts = document.querySelectorAll(".post")
+
+selectorTecnologia.addEventListener('change', () => {
+    posts.forEach(post => {
+        post.classList = "post"
+        
+    })
+    posts.forEach(post => {
+        let contenido = post.querySelector(".contenido").textContent   
+        if(contenido.includes(String(selectorTecnologia.value)) === false) post.classList.add("filtered")
+        console.log(contenido)
+    })
+})
+
